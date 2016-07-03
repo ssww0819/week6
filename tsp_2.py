339b057783494b875a7cb729e48893bc10a9b9bb
ssww0819 <shaowen0819@yahoo.co.jp>
2016-07-03 15:52:49 +0900

    greedy

---
+++ tsp_2.py
@@ -0,0 +1,44 @@
+import math
+import sys
+import random
+
+def distance(city1, city2):
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
+def read_input(filename):
+    with open(filename) as f:
+        cities = []
+        for line in f.readlines()[1:]:  # Ignore the first line.
+            xy = line.split(',')
+            cities.append((float(xy[0]), float(xy[1])))
+        return cities
+
+
+if __name__ == "__main__":
+    param = sys.argv
+
+cities = read_input (param[1])
+n = len(cities)
+route = [0] * n
+sum_distance = 0
+visited = [True] * n
+visited[0] = False
+
+x = route[0]
+MAX = 50000
+for i in range(1, n):
+    mini = MAX
+    for a in range(1, n):
+        if visited[a] == True:
+            if distance(cities[a], cities[route[i-1]]) < mini:
+                route[i] = a
+                mini = distance(cities[a], cities[route[i-1]])
+    visited[route[i]] = False
+                
+for i in range(0, n-1):
+    sum_distance += distance(cities[route[i]], cities[route[i+1]])
+
+sum_distance += distance(cities[route[n-1]], cities[route[0]])
+
+print route
+print sum_distance
